27a7b3152714de835ee96679584e17dd50a1a075
Shashi Kumar M <shashikumar.m@gieom.com>
2025-06-24 02:00:56 +0530

    video categories added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,21 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import Client from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-export default async function Home() {
-  void trpc.hello.prefetch({ text: " Shashi!" });
+export const dynamic = "force-dynamic"; // This page should always be re-rendered on the server
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>
+}
+
+const  Page=async({ searchParams }: PageProps) =>{
+  const  { categoryId } = await searchParams;
+  void trpc.categories.getMany.prefetch();
   return (
     <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<p>Error occurred</p>}>
-          <Client />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
 }
+
+export default Page;
