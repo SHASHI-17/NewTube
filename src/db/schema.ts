1fb54c3579011743b7e024a2a6884672309feeeb
Shashi Kumar M <shashikumar.m@gieom.com>
2025-07-05 19:17:56 +0530

    getMany Videos procedure added

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   pgTable,
   text,
@@ -19,6 +20,10 @@ export const users = pgTable(
   (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
 );
 
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
+
 export const categories = pgTable(
   "categories",
   {
@@ -30,3 +35,48 @@ export const categories = pgTable(
   },
   (t) => [uniqueIndex("name_idx").on(t.name)]
 );
+
+export const categoryRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  description: text("description"),
+
+  // muxAssetId: text("mux_asset_id").unique(),
+  // muxStatus: text("mux_status"),
+  // muxUploadId: text("mux_upload_id").unique(),
+  // muxPlaybackId: text("mux_playback_id").unique(),
+  // muxTrackId: text("mux_track_id").unique(),
+  // muxTrackStatus: text("mux_track_status"),
+  // thumbnailUrl: text("thumbnail_url"),
+  // thumbnailKey: text("thumbnail_key"),
+  // previewUrl: text("preview_url"),
+  // previewKey: text("preview_key"),
+  // duration: integer("duration").default(0).notNull(),
+  // visibility: videoVisibility("visibility").default("private").notNull(),
+
+  userId: uuid("user_id")
+    .references(() => users.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  categoryId: uuid("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}));
